3f896461016a1a742c0bd2c26934f322a255dae9
ismeet010 <ismeetchhabda@gmail.com>
2023-08-07 17:01:49 +0530

    basic components

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <div class="col-1">
+        <h3>Useful links</h3>
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#menu">Menu</a>
+      </div>
+      <div class="col-2">
+        <h3>Contact us</h3>
+        <p>address...India</p>
+      </div>
+      <div class="col-3">
+        <h3>Connect with us</h3>
+        <p></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" width={"200px"} height={"70px"}></img>
+    </header>
+  );
+}
+
+export default Header;
