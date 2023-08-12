9a54da3c4b847b0d1ee08e5cc0d2ba4afe07183d
ismeet010 <ismeetchhabda@gmail.com>
2023-08-12 22:20:37 +0530

    navbar fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import logo from "../assets/logo.png";
+import Nav from "./Nav";
 
 function Header() {
   return (
     <header>
-      <img src={logo} alt="logo" width={"200px"} height={"70px"}></img>
+      {/* <img src={logo} alt="logo" width={"200px"} height={"70px"}></img> */}
+      <Nav />
     </header>
   );
 }
