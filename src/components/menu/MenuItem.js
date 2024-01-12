7d10a177b240017d9871f6394cd691aad28e9d0f
ismeet010 <ismeetchhabda@gmail.com>
2024-01-12 23:39:30 +0530

    added menu items

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const MenuItem = () => {
+const MenuItem = (props) => {
   return (
-    <div className='flex-column menu-width'>
+    <div className='flex-column menu-width margin-bottom'>
       <div className="flex-row space-between bottomborder">
-        <h2>dish name</h2>
-        <span>Price</span>
+        <h2>{props.name}</h2>
+        <span>{props.price}</span>
       </div>
-      <p>jhfyukf</p>
+      <p>{props.desc}</p>
     </div>
   );
 }
